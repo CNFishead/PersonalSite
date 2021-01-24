f6bdd002ef53ef5e05e77d156ab10f4778c22773
CNFishead <76625097+CNFishead@users.noreply.github.com>
2021-01-24 17:54:37 -0500

    Add files via upload

--- index.js
+++ index.js
@@ -2,3 +2,50 @@ $("h1.clickMeButton").on("click", function(){
   $("img.opening-page").fadeOut();
   $("h1.clickMeButton").fadeOut();
 });
+
+$(".owl-carousel").owlCarousel({
+   responsiveClass: true,
+   dots: true,
+   dotsContainer: false,
+      loop: true,
+      autoWidth: false,
+     autoplay:true,
+     nav:true,
+     smartSpeed:1000,
+     items:5,
+     responsive: {
+       0: {
+          items:1,
+          // margin: 150,
+          center: true
+
+          },
+
+       768: {
+            items:1,
+            // margin:200,
+            center: false
+            },
+
+       992: {
+            items:2,
+            // margin: 35,
+            center: true
+            },
+
+       1200: {
+             items:3,
+             // margin:250,
+             center: true
+             },
+
+                  },
+ });
+
+ // set height of all cards to max height
+ const cards = Array.from(document.querySelectorAll('.card'));
+ const cardHeights = cards.map(card => card.offsetHeight);
+ const maxCardHeight = Math.max(...cardHeights);
+ cards.forEach(card => {
+   card.style.height = maxCardHeight + 'px';
+ });
